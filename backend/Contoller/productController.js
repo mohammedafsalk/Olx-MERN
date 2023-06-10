fb2f17a69a50b7e81b791dd163f9b2dd5b21bc71
mohammedafsalk <techafs456@gmail.com>
2023-06-10 12:34:15 +0530

    backend completed

--- backend/Contoller/productController.js
+++ backend/Contoller/productController.js
@@ -22,7 +22,7 @@ export async function addProduct(req, res) {
   try {
     const image = req.file;
     const product = await Products.create({ ...req.body, image });
-    res.json({ error: false});
+    res.json({ error: false });
   } catch (error) {
     res.json({ error: error.message });
   }
@@ -31,6 +31,22 @@ export async function addProduct(req, res) {
 export async function getItem(req, res) {
   try {
     const item = await Products.findById(req.params.id);
+    let user;
+    if (item) {
+      user = await User.findById(item.userId);
+    }
+    res.json({ error: false, item, user });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+}
+
+export async function getCategories(req, res) {
+  try {
+    const categories = await Products.aggregate([
+      { $group: { _id: "$category" } },
+    ]);
+    res.json({ error: false, categories });
   } catch (error) {
     res.json({ error: error.message });
   }
--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,15 +1,41 @@
 import express from "express";
-import { Signup, auth, home, login, logout } from "../Contoller/userController.js";
-import { getProducts } from "../Contoller/productController.js";
+import multer from "multer";
+import {
+  Signup,
+  auth,
+  home,
+  login,
+  logout,
+} from "../Contoller/userController.js";
+import {
+  addProduct,
+  getCategories,
+  getItem,
+  getProducts,
+} from "../Contoller/productController.js";
 const router = express.Router();
 
-router.get('/',home)
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + ".jpg";
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/", home);
 router.post("/signup", Signup);
 router.post("/login", login);
 router.get("/auth", auth);
 router.get("/logout", logout);
 
-router.get('/products',getProducts)
-router.get('/products/:id',)
+router.get("/products", getProducts);
+router.post("/add-product", upload.single("image"), addProduct);
+router.get("/products/:id", getItem);
+router.get("/categories", getCategories);
 
 export default router;
